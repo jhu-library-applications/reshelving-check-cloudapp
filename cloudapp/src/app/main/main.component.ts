5498844e9e4972598ae1cf9e8d749256a22ad26e
Stephen Cassidy <scassid6@jhu.edu>
2024-02-12 16:34:11 -0500

    Remove unnecessary code

--- cloudapp/src/app/main/main.component.ts
+++ cloudapp/src/app/main/main.component.ts
@@ -17,12 +17,8 @@ import { ItemUpdate } from '../interfaces/item_update.interface';
 export class MainComponent implements OnInit, OnDestroy {
   loading = false;
   itemRMSTMatch: boolean = false;
-  uniqueItemIds: Set<string> = new Set();
-  itemList: Item[] = [];
   itemRecord: Item;
   rmstBarcodeForItems: string;
-  locationCheck: boolean = false;
-  bigMoveMode: boolean = false;
   // Sandbox values 
   // library: string = 'elsc';
   // location: string = '2305open';
@@ -52,6 +48,9 @@ export class MainComponent implements OnInit, OnDestroy {
     if(this.itemRecord && this.itemRecord.item_data.storage_location_id === this.rmstBarcodeForItems) {
       this.itemRMSTMatch = true;
       return;
+    } else {
+      this.playBeep("C3");
+      this.alert.error('The RMST barcode does not match the item.');
     }
   }
 
@@ -63,13 +62,6 @@ export class MainComponent implements OnInit, OnDestroy {
     ).subscribe(
       item => {
         const uniqueId = item.item_data.barcode;
-
-        if (this.locationCheck && item.item_data.library.value != this.library) {
-          this.playBeep("C3");
-          this.alert.error(`Item with the barcode ${itemBarcode} is not in the ${this.library} library.`);
-          return;
-        }
-
         this.itemRecord = item;
         this.checkItem();
       },
@@ -88,48 +80,10 @@ export class MainComponent implements OnInit, OnDestroy {
 
   onSubmit() {
     this.loading = true;
-
-    const updateRequests = this.itemList.map(item => {
-      const itemUpdate: ItemUpdate = {
-        item: item,
-        rmstBarcodeForItems: this.rmstBarcodeForItems,
-        bigMoveMode: this.bigMoveMode,
-        location: this.location,
-        library: this.library,
-        libraryDesc: this.libraryDesc,
-        locationDesc: this.locationDesc,
-      };
-
-      return this.itemService.updateItem(itemUpdate);
-    });
-
-    forkJoin(updateRequests).subscribe({
-      next: (response) => {
-        this.loading = false;
-        this.itemList = [];
-        this.uniqueItemIds = new Set();
-        this.rmstBarcodeForItems = undefined;
-
-        this.alert.success('RMST added to all items successfully.');
-      },
-      error: (error) => {
-        this.loading = false;
-        console.error(error);
-        this.alert.error('An error occurred while updating the items: ' + error.message);
-      }
-    });
+    this.checkItem();
   }
-
-  removeItem(item: Item): void {
-    this.itemList = this.itemList.filter(currentItem => currentItem !== item);
-
-    const uniqueId = item.bib_data.mms_id;
-    this.uniqueItemIds.delete(uniqueId);
-  }
-
   reset(): void {
-    this.itemList = [];
-    this.uniqueItemIds = new Set();
+    this.itemRecord = undefined;
     this.rmstBarcodeForItems = undefined;
   }
 }
